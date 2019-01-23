76dc9cffef4825582358fab9e63ced0886a0c916
Ivo van Doesburg <ivo@fourohtwo.nl>
2019-01-23 14:24:34 +0100

    include a 4x4 matrix of dominant color in each quadrant

--- nmt_filters/meta_monkey.py
+++ nmt_filters/meta_monkey.py
@@ -12,7 +12,9 @@ Ceci n'est pas une filter
 This file patches the `JSONEngine.read` method to include color information
 when the meta endpoint is called.
 
-Example: http://localhost:8888/unsafe/meta/https://images.nrc.nl/qNGHVRrH63Npu-vdTUplQxGm8P8=/320x192/smart/filters:no_upscale()/s3/static.nrc.nl/wp-content/uploads/2019/01/web-1501buiafdjpg.jpg
+Example: 
+- http://localhost:8888/unsafe/meta/static.nrc.nl/wp-content/uploads/2019/01/web-1501buiafdjpg.jpg
+- http://localhost:8888/unsafe/meta/static.nrc.nl/wp-content/uploads/2019/01/web-1501buiafdjpg.jpg?callback=handleMeta
 
 It is implemented as a filter as these can be configured in config `FILTERS` and are
 conveniently imported at server startup in thumbor.server.main by `get_importer`
@@ -23,6 +25,18 @@ To enable add 'nmt_filters.meta_monkey' at the top of `FILTERS` in the thumbor c
 
 class Filter(BaseFilter):
     pass
+
+
+def get_matrix(matrix_image):
+    image = matrix_image.convert("RGBA")
+    width, height = image.size
+    pixels = image.getdata()
+    pixel_count = width * height
+    matrix = []
+    for i in range(0, pixel_count):
+        r, g, b, a = pixels[i]
+        matrix.append([r, g, b])
+    return matrix
 
 
 def monkey_read(self, extension, quality):
@@ -44,10 +58,17 @@ def monkey_read(self, extension, quality):
         thumbor_json["thumbor"]["focal_points"] = self.focal_points
 
     # use a resampled image for performance
-    thumbnail_image = self.image.resize((320, 240), Image.NEAREST)
+    palette_ct = ColorThief(self.image.resize((320, 240), Image.NEAREST))
+    matrix_image = self.image.resize((2, 2), Image.NEAREST)
 
-    ct = ColorThief(thumbnail_image)
-    thumbor_json["colors"] = dict(dominant=ct.get_color(), palette=ct.get_palette())
+    thumbor_json["colors"] = dict(
+        # the dominant color:
+        dominant=palette_ct.get_color(),
+        # a palette of the most dominant colors
+        palette=palette_ct.get_palette(),
+        # a 4x4 matrix of the dominant color in each quadrant of the image
+        matrix=(get_matrix(matrix_image)),
+    )
     thumbor_json = json.dumps(thumbor_json)
 
     if self.callback_name:
